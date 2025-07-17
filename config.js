26f6ee85e27ec4781bd81220598f496fc82a81a5
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-07-17 12:49:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -71,9 +71,9 @@ PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
+READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
-DEV: process.env.DEV || "918279975767",
+DEV: process.env.DEV || "918292001437",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
